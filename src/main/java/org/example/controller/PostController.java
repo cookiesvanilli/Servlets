5fb696c3220e9ed56aa2a24f8655b448195feea8
Inna Kuklina <in.d2010@yandex.ru>
2024-10-27 16:06:44 +0300

    Initial commit

---
+++ src/main/java/org/example/controller/PostController.java
@@ -0,0 +1,45 @@
+package org.example.controller;
+
+import com.google.gson.Gson;
+import org.example.service.PostService;
+import org.example.model.Post;
+
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Reader;
+
+public class PostController {
+    public static final String APPLICATION_JSON = "application/json";
+    private final PostService service;
+    private final Gson gson;
+
+    public PostController(PostService service) {
+        this.service = service;
+        gson = new Gson();
+    }
+
+    public void all(HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        final var data = service.all();
+        response.getWriter().print(gson.toJson(data));
+    }
+
+    public void getById(long id, HttpServletResponse response) throws IOException {
+        final var data = service.getById(id);
+        response.setContentType(APPLICATION_JSON);
+        response.getWriter().print(gson.toJson(data));
+    }
+
+    public void save(Reader body, HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        final var post = gson.fromJson(body, Post.class);
+        final var data = service.save(post);
+        response.getWriter().print(gson.toJson(data));
+    }
+
+    public void removeById(long id, HttpServletResponse response) throws IOException {
+        service.removeById(id);
+        response.getWriter().print("Post with id#" + id + " deleted successful");
+    }
+}
---
+++ src/main/java/org/example/repository/PostRepository.java
@@ -0,0 +1,60 @@
+package org.example.repository;
+
+import org.example.exception.NotFoundException;
+import org.example.model.Post;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+
+// Stub
+public class PostRepository {
+
+    private Map<Long, Post> postsListMap = new ConcurrentHashMap<>();
+    private AtomicLong postCount = new AtomicLong(0);
+
+
+    public List<Post> all() {
+        // to convert map values to list
+        return postsListMap.values().
+                stream().collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public Optional<Post> getById(long id) {
+
+        for (Post post : postsListMap.values()) {
+            if (post.getId() == id) {
+                return Optional.of(post);
+            }
+        }
+        return Optional.empty();
+
+    }
+
+    public Post save(Post post) {
+
+        if (post.getId() != 0) {
+            if ((postsListMap.containsKey(post.getId()))) {
+                postsListMap.put(post.getId(), post);
+            } else {
+                throw new NotFoundException("Post not saved {id:" + post.getId() + "}");
+            }
+        } else {
+            // post.getId() == 0
+            long id = postCount.incrementAndGet();
+            post.setId(id);
+            postsListMap.put(id, post);
+        }
+        return post;
+    }
+
+    public void removeById(long id) {
+
+        if (postsListMap.containsKey(id)) {
+            postsListMap.remove(id);
+        } else {
+            throw new NotFoundException("Post not found {id:" + id + "}.Please check the id and try again.");
+        }
+    }
+}
